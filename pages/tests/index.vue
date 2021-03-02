22f4f63debb18666b64a56d378906e4a55849d07
ParkYoungWoong <thesecon@gmail.com>
2021-03-02 14:18:16 +0900

    Create project

---
+++ pages/tests/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hello Vercel</h1>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData () {
+    // let res = {}
+    const { data } = await axios.get('https://www.omdbapi.com/?i=tt3896198&apikey=7035c60c')
+    console.log(data)
+    return {
+      res: data
+    }
+  },
+  head () {
+    const { Title, Year } = this.res
+    return {
+      title: `${Title} / ${Year}`
+    }
+  }
+}
+</script>
